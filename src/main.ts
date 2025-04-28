dd19a90e766fe577017bf4b25eee62d58e77afa0
mahindar5 <mahindar.275@gmail.com>
2025-04-27 22:50:23 -0700

    feat: update angular.json and main.ts for improved build configuration and routing setup chore: add VSCode settings for consistent code formatting

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 import { BUILD_INFO as commonLibBUILD_INFO, fetchProxy } from '@mahindar5/common-lib';
-import { configureApplicationWithConfig, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
+import { configureApplication, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
 import { AppComponent, } from '@mahindar5/ng-common-mat-lib/components/app';
 import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
 
+
+export const routes: Routes = [
+	{ path: '', redirectTo: 'vsc', pathMatch: 'full' },
+	{
+		path: 'vsc',
+		loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/json-uploader').then(m => m.JsonUploaderComponent),
+	},]
 fetchProxy();
-bootstrapApplication(AppComponent, configureApplicationWithConfig({ BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO } }))
+bootstrapApplication(AppComponent, configureApplication(routes, { BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO } }))
 	.catch((err) => console.error(err));
