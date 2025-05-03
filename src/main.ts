d57b1f147058479693466d85f523b1bfd48e9b3f
mahindar5 <mahindar.275@gmail.com>
2025-05-03 00:21:16 -0700

    refactor: update main.ts to use PromptBatchRunnerComponent and clean up tsconfig.json formatting

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
 import { BUILD_INFO as commonLibBUILD_INFO, fetchProxy } from '@mahindar5/common-lib';
 import { configureApplication, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
-import { AppComponent, } from '@mahindar5/ng-common-mat-lib/components/app';
+import { PromptBatchRunnerComponent } from '@mahindar5/ng-common-mat-lib/components/prompt-batch-runner';
 import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
 
 
-export const routes: Routes = [
-	{
-		path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/json-uploader').then(m => m.JsonUploaderComponent),
-	},]
 fetchProxy();
-bootstrapApplication(AppComponent, configureApplication(routes, { BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO } }))
-	.catch((err) => console.error(err));
+bootstrapApplication(PromptBatchRunnerComponent, await configureApplication([], {
+	BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO },
+	excludeAIService: true,
+	hideMenuIcon: true,
+})).catch((err) => console.error(err));
