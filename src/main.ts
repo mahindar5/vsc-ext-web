1f5996262476e0a05f1dafbf38a64031a2e742c2
mahindar5 <mahindar.275@gmail.com>
2025-05-03 00:37:16 -0700

    refactor: update main.ts to use routes for PromptBatchRunnerComponent and improve application bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 import { BUILD_INFO as commonLibBUILD_INFO, fetchProxy } from '@mahindar5/common-lib';
 import { configureApplication, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
-import { PromptBatchRunnerComponent } from '@mahindar5/ng-common-mat-lib/components/prompt-batch-runner';
+import { AppComponent, } from '@mahindar5/ng-common-mat-lib/components/app';
 import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
 
 
+export const routes: Routes = [
+	{
+		path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/prompt-batch-runner').then(m => m.PromptBatchRunnerComponent),
+	},]
 fetchProxy();
-bootstrapApplication(PromptBatchRunnerComponent, await configureApplication([], {
+bootstrapApplication(AppComponent, await configureApplication(routes, {
 	BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO },
 	excludeAIService: true,
 	hideMenuIcon: true,
-})).catch((err) => console.error(err));
+}))
+	.catch((err) => console.error(err));
