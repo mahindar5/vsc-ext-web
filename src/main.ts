d6318e6fe54cd035db5cf04d361de42848b43dcc
mahindar5 <mahindar.275@gmail.com>
2025-05-03 00:37:44 -0700

    refactor: clean up routes definition in main.ts for better readability

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ import { AppComponent, } from '@mahindar5/ng-common-mat-lib/components/app';
 import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
 
 
-export const routes: Routes = [
-	{
-		path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/prompt-batch-runner').then(m => m.PromptBatchRunnerComponent),
-	},]
+export const routes: Routes = [{
+	path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/prompt-batch-runner').then(m => m.PromptBatchRunnerComponent),
+}]
 fetchProxy();
 bootstrapApplication(AppComponent, await configureApplication(routes, {
 	BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO },
