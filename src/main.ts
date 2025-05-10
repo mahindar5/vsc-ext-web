74d5394729a7a5410b63d003e04e5b1225d62fb6
mahindar5 <mahindar.275@gmail.com>
2025-05-10 11:36:25 -0700

    feat: add azure-dark-theme class to index.html and update main.ts to use PromptBatchRunnerComponent

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
 import { BUILD_INFO as commonLibBUILD_INFO, fetchProxy } from '@mahindar5/common-lib';
 import { configureApplication, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
-import { AppComponent, } from '@mahindar5/ng-common-mat-lib/components/app';
+import { PromptBatchRunnerComponent } from '@mahindar5/ng-common-mat-lib/components/prompt-batch-runner';
 import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
 
 
-export const routes: Routes = [{
-	path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/prompt-batch-runner').then(m => m.PromptBatchRunnerComponent),
-}]
+// export const routes: Routes = [{
+// 	path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/prompt-batch-runner').then(m => m.PromptBatchRunnerComponent),
+// }]
 fetchProxy();
-bootstrapApplication(AppComponent, await configureApplication(routes, {
+bootstrapApplication(PromptBatchRunnerComponent, await configureApplication([], {
 	BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO },
 	excludeAIService: true,
 	hideMenuIcon: true,
