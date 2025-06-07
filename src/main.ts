701328f79544420d9b4ca7dff7e4630bd7896870
mahindar5 <mahindar.275@gmail.com>
2025-06-07 09:17:37 -0700

    refactor: remove azure-dark-theme class from index.html and update main.ts to use AppComponent

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 import { fetchProxy } from '@mahindar5/common-lib';
 import { configureApplication } from '@mahindar5/ng-common-mat-lib';
-import { PromptBatchRunnerComponent } from '@mahindar5/ng-common-mat-lib/components/prompt-batch-runner';
+import { AppComponent } from '@mahindar5/ng-common-mat-lib/components/app';
 import { BUILD_INFO } from './build-info';
 
-
-// export const routes: Routes = [{
-// 	path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/prompt-batch-runner').then(m => m.PromptBatchRunnerComponent),
-// }]
 fetchProxy();
-bootstrapApplication(PromptBatchRunnerComponent, await configureApplication([], {
+// Importing AppComponent
+export const routes: Routes = [{
+	path: '', loadComponent: () => import('@mahindar5/ng-common-mat-lib/components/prompt-batch-runner').then(m => m.PromptBatchRunnerComponent),
+}]
+bootstrapApplication(AppComponent, await configureApplication(routes, {
 	BUILD_INFO: { myAppBUILD_INFO: BUILD_INFO },
 	excludeAIService: true,
-	hideMenuIcon: true,
 })).catch((err) => console.error(err));
+
+// Direct import of PromptBatchRunnerComponent and add class="azure-dark-theme" to index.html
+// bootstrapApplication(PromptBatchRunnerComponent, await configureApplication([], {
+// 	BUILD_INFO: { myAppBUILD_INFO: BUILD_INFO },
+// 	excludeAIService: true,
+// 	hideMenuIcon: true,
+// })).catch((err) => console.error(err));
