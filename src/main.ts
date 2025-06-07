7ceb8e94a5a3be63e2734756e9f9b0793360bf5e
mahindar5 <mahindar.275@gmail.com>
2025-06-07 01:05:17 -0700

    refactor: simplify BUILD_INFO imports and usage in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { BUILD_INFO as commonLibBUILD_INFO, fetchProxy } from '@mahindar5/common-lib';
-import { configureApplication, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
+import { fetchProxy } from '@mahindar5/common-lib';
+import { configureApplication } from '@mahindar5/ng-common-mat-lib';
 import { PromptBatchRunnerComponent } from '@mahindar5/ng-common-mat-lib/components/prompt-batch-runner';
-import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
+import { BUILD_INFO } from './build-info';
 
 
 // export const routes: Routes = [{
@@ -10,7 +10,7 @@ import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
 // }]
 fetchProxy();
 bootstrapApplication(PromptBatchRunnerComponent, await configureApplication([], {
-	BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO },
+	BUILD_INFO: { myAppBUILD_INFO: BUILD_INFO },
 	excludeAIService: true,
 	hideMenuIcon: true,
 })).catch((err) => console.error(err));
